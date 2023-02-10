7129c5c0196f32e21fa51d1df713949c3f9d15f8
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-10 15:01:27 +0700

    Add files via upload

---
+++ strings/helpers.py
@@ -0,0 +1,116 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+HELP_1 = """✅**<u>Admin Commands:</u>**
+
+**c** adalah singkatan dari pemutaran saluran.
+
+/pause atau /cpause - Menjeda musik yang sedang diputar.
+/resume atau /cresume- Melanjutkan musik yang dijeda.
+/mute atau /cmute- Mematikan musik yang diputar.
+/unmute atau /cunmute- Mengaktifkan musik yang dimatikan.
+/skip atau /cskip- Lewati musik yang sedang diputar.
+/stop atau /cstop- Menghentikan pemutaran musik.
+/shuffle atau /cshuffle- Secara acak mengacak daftar putar yang antri.
+/seek atau /cseek - Teruskan Cari musik sesuai durasi Anda
+/seekback atau /cseekback - Mundur Mencari musik sesuai durasi Anda
+/restart - Mulai ulang bot untuk obrolan Anda .
+
+
+✅<u>**Specific Skip:**</u>
+/skip atau /cskip [Nomor(contoh: 3)]
+     - Melompati musik ke nomor antrian yang ditentukan. Contoh: /skip 3 akan melewatkan musik ke musik antrian ketiga dan akan mengabaikan musik 1 dan 2 dalam antrian.
+
+✅<u>**Loop Play:**</u>
+/loop or /cloop [enable/disable] or [Angka antara 1-10] 
+    - Saat diaktifkan, bot memutar musik yang sedang diputar menjadi 1-10 kali pada obrolan suara. Default ke 10 kali.
+
+✅<u>**Auth Users:**</u>
+Pengguna Auth dapat menggunakan perintah admin tanpa hak admin di obrolan Anda.
+
+/auth [Username] - Tambahkan pengguna ke AUTH LIST grup.
+/unauth [Username] - Hapus pengguna dari AUTH LIST grup.
+/authusers - Periksa DAFTAR AUTH grup."""
+
+
+HELP_2 = """✅<u>**Play Commands:**</u>
+
+Perintah yang tersedia = play , vplay , cplay
+
+Perintah ForcePlay = playforce , vplayforce , cplayforce
+
+**c** singkatan dari pemutaran di Chanell.
+**v** singkatan dari pemutaran video.
+**force** singkatan dari force play.
+
+/play or /vplay or /cplay  - Bot akan mulai memainkan kueri yang Anda berikan di obrolan suara atau Streaming tautan langsung di obrolan suara.
+
+/playforce or /vplayforce or /cplayforce -  **Force Play** menghentikan trek yang sedang diputar di obrolan suara dan mulai memutar trek yang dicari secara instan tanpa mengganggu/clearing queue.
+
+/channelplay [Chat username or id] or [Disable] - Hubungkan saluran ke grup dan streaming musik di obrolan suara saluran dari grup Anda.
+
+
+✅**<u>Bot's Server Playlists:</u>**
+/playlist  - Periksa Daftar Putar Tersimpan Anda Di Server.
+/deleteplaylist - Hapus semua musik yang disimpan di daftar putar Anda
+/play  - Mulai mainkan Daftar Putar Tersimpan Anda dari Server."""
+
+
+HELP_3 = """✅<u>**Bot Commands:**</u>
+
+/stats - Dapatkan 10 Trek Global Stats Teratas, 10 Pengguna Bot Teratas, 10 Obrolan Teratas di bot, 10 Teratas Dimainkan dalam obrolan, dll..
+
+/sudolist - Periksa Sudo Pengguna Amang Music Bot
+
+/lyrics [Music Name] - Mencari Lirik untuk Musik tertentu di web.
+
+/song [Track Name] or [YT Link] - Unduh lagu apa pun dari youtube dalam format mp3 atau mp4.
+
+/player -  Dapatkan Panel Bermain interaktif.
+
+**c** stands for channel play.
+
+/queue or /cqueue- Check Queue List of Music."""
+
+HELP_4 = """✅<u>**Extra  Commands:**</u>
+/start - Mulai Bot Musik.
+/help - Dapatkan Menu Helper Perintah dengan penjelasan rinci tentang perintah.
+/ping- Ping Bot dan periksa statistik Ram, Cpu, dll dari Bot.
+
+✅<u>**Group Settings:**</u>
+/settings - Dapatkan pengaturan grup lengkap dengan tombol sebaris
+
+🔗 **Options in Settings:**
+
+1️⃣ Anda dapat mengatur **Kualitas Audio** yang ingin Anda streaming di obrolan suara.
+
+2️⃣ Anda dapat mengatur **Kualitas Video** yang ingin Anda streaming di obrolan suara.
+
+3️⃣ **Pengguna Auth**:- Anda dapat mengubah mode perintah admin dari sini ke semua orang atau hanya admin. Jika semua orang, siapa pun yang ada di grup Anda dapat menggunakan perintah admin (seperti /skip, /stop dll)
+
+4️⃣ **Mode Bersih:** Saat diaktifkan, hapus pesan bot setelah 5 menit dari grup Anda untuk memastikan obrolan Anda tetap bersih dan baik.
+
+5️⃣ **Perintah Bersih** : Saat diaktifkan, Bot akan segera menghapus perintah yang dijalankannya (/play, /pause, /shuffle, /stop dll).
+
+6️⃣ **Pengaturan Putar:**
+
+/playmode - Dapatkan panel pengaturan pemutaran lengkap dengan tombol di mana Anda dapat mengatur pengaturan pemutaran grup Anda.
+
+<u>Opsi dalam mode putar:</u>
+
+1️⃣ **Mode Pencarian** [Langsung atau Sebaris] - Mengubah mode pencarian Anda saat Anda memberikan mode /play.
+
+2️⃣ **Perintah Admin** [Semua Orang atau Admin] - Jika semua orang, siapa pun yang hadir di grup Anda akan dapat menggunakan perintah admin (seperti /skip, /stop dll)
+
+3️⃣ **Tipe Putar** [Semua Orang atau Admin] - Jika admin, hanya admin yang ada di grup yang dapat memutar musik di obrolan suara."""
+
+HELP_5 = """
+-KETIK /help DIBAWAH UNTUK MELIHAT COMMAND MANAGE AMANGROBOT
+-TYPE /help BELOW TO VIEW THE AMANGROBOT COMMAND MANAGE
+"""
