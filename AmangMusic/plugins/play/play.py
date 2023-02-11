dd32daf634409837e36d13221195d90939f522f6
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-11 20:08:26 +0700

    Update play.py

--- AmangMusic/plugins/play/play.py
+++ AmangMusic/plugins/play/play.py
@@ -600,7 +600,7 @@ async def anonymous_check(client, CallbackQuery):
 
 
 @app.on_callback_query(
-    filters.regex("AmangMusicPlaylists") & ~BANNED_USERS
+    filters.regex("AmangPlaylists") & ~BANNED_USERS
 )
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
